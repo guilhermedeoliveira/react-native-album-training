4c4d2a83441a2eb0e9272bf25191d6fe3d551957
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-06-19 14:13:52 -0300

    Fetch API albums

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
+  
+
+  componentWillMount() {
+      fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => response.json())
+      .then(data => this.setState({ albums: data }));
+      console.log(this.state);
+  }
+
   render() {
     return (
       <View>
