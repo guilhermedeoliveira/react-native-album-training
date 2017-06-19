eb896445ed86124ac8bd8b293964b7d656e5d0b2
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-06-19 16:53:30 -0300

    Render AlbumDetail inside AlbumList

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.state = { albums: [] };
+  }
 
   componentWillMount() {
       fetch('https://rallycoding.herokuapp.com/api/music_albums')
@@ -11,10 +17,16 @@ class AlbumList extends Component {
       console.log(this.state);
   }
 
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    );
+  }
+
   render() {
     return (
       <View>
-        <Text>AlbumList</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
