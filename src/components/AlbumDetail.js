b95646e79614d21647a4e17bd43d4beae7558768
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-06-21 13:25:19 -0300

    Apply styles in AlbumDetail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -6,21 +6,49 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
   const { thumbnail_image, title, artist } = album;
+  const {
+    headerContentStyle,
+    headerTextStyle,
+    thumbnailStyle,
+    thumbnailContainerStyle
+  } = styles;
 
   return (
     <Card>
       <CardSection>
-        <View>
-          <Image source={{ uri: thumbnail_image }} />
+        <View style={thumbnailContainerStyle}>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
         </View>
-        <View>
-          <Text>{title}</Text>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
-
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  }
 };
 
 export default AlbumDetail;
