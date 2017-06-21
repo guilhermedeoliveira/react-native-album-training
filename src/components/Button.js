78a4a925a7de326c649354736d0cab61fa618793
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-06-21 17:13:11 -0300

    Organize Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 
-const Button = (props) => {
+const Button = () => {
+  const { buttonStyle, textStyle } = styles;
+
   return (
-    <TouchableOpacity>
-      <Text>Click</Text>
+    <TouchableOpacity style={buttonStyle}>
+      <Text style={textStyle}>Click</Text>
     </TouchableOpacity>
-  )
+  );
 };
 
 const styles = {
@@ -19,6 +21,14 @@ const styles = {
     borderColor: '#007aff',
     marginLeft: 5,
     marginRight: 5
+  },
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
   }
 };
 
